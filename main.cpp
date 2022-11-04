580bc2bc195bf8ca3b0a97e2bdf507898bbda405
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-11-03 23:24:28 -0400

    added solve function example

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 Author: Ashlee Tolentino
 Course: CSCI-135
 Instructor: Michael Zamansky
-Assignment: Lab8
+Assignment: Lab-cipher
 
-This program contains functions that...
+This program uses the techniques we discussed in class to decode the parameter encrypted_string. That string will be text that has been encoded using Caesar cipher.
 */
 
 #include <iostream>
@@ -15,26 +15,15 @@ This program contains functions that...
 
 int main()
 {
-  std::string input = "Hello, World!";
+  std::string input = "What if a writer or novelist decided to write a 300-page book with no breaks in the text for new ideas, new chapters, or even character dialogue? It would be so overwhelming that the reader would probably take one look and close the book forever. When writing is divided up into manageable parts that are cohesive, it's much easier for the reader to take in and process. A paragraph is a distinct segment of writing, often includes more than one sentence, and is separated from other paragraphs and text by a space. Paragraphs break up text into manageable chunks that are easily read and visually make it more appealing and less daunting. The word paragraph comes from the Latin word paragraphos, which is roughly translated to mean a short-stroke marking a break in sense. The term graph is Latin for writing. There is often some confusion about what the difference is between a paragraph and an essay. The best way to understand the difference is to think of the essay as a bigger version of a paragraph. A paragraph is made up of a topic sentence, supporting sentences, and a concluding sentence. An essay has all the same parts; only instead of sentences, an essay is made up of paragraphs.";
 
   std::vector<double> solved_frequency(26);
   solved_frequency = solve_frequency(input);
 
-  /*
-  for(int i = 0; i < 26; i++)
-  {
-    std::cout << solved_frequency.at(i) << std::endl;
-  }
-  */
-
   std::string encrypted_string = encryptCaesar(input, 10);
-  //std::cout << encrypted_string << std::endl;
-  
 
   std::string decrypted_string = solve(encrypted_string);
-  std::cout << decrypted_string << std::endl;
+  std::cout << decrypted_string << std::endl;  
   
   return 0;
 }
-
-//count spaces and then substract it from the length of sentence
