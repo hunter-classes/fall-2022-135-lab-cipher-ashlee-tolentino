6bc55b4af18feeecad83fa0ce0a0818ed7c4dff5
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-11-03 23:26:28 -0400

    fixed some errors

--- funcs.cpp
+++ funcs.cpp
@@ -2,9 +2,9 @@
 Author: Ashlee Tolentino
 Course: CSCI-135
 Instructor: Michael Zamansky
-Assignment: Lab8
+Assignment: Lab-cipher
 
-This program contains functions that...
+This program uses the techniques we discussed in class to decode the parameter encrypted_string. That string will be text that has been encoded using Caesar cipher.
 */
 
 #include <iostream>
@@ -13,36 +13,26 @@ This program contains functions that...
 #include "funcs.h"
 #include "caesar.h"
 
-//std::vector<double> solve_frequency(std::string);
-
 // This function decodes the encrypted string
 std::string solve(std::string encrypted_string)
 {
-  /*
-  std::vector<double> letter_frequency(26);
-  letter_frequency = solve_frequency("It is important to note that a paragraph does not have a minimum or maximum number of sentences that it must have to fit the definition of a paragraph. Some writers will opt to use very short paragraphs, while others will include dozens of sentences in their paragraphs. It is also important to know that most writers separate lines of dialogue into paragraphs, so if a character only speaks a single line, it will be its own paragraph. Keeping that in mind, there is a general agreement on the format of a standard paragraph, which especially applies to informational and argumentative or persuasive writing. A paragraph should be divided into three distinct sections that each serve a purpose to the paragraph as a whole.");
-  */
-  
   double letter_frequency[] =
   {
-    .084966, .020720, .045388, .033844, .111607,
-    .018121, .024705, .030034, .075448, .001964,
-    .011016, .054893, .030129, .066544, .071635,
-    .031671, .001962, .075809, .057351, .069509,
-    .036308, .010074, .012899, .002902, .017779,
-    .002722
+    0.0768837, 0.0144405, 0.0266625, 0.0484597, 0.132462,
+    0.0250848, 0.0172021, 0.0567935, 0.0706592, 0.00144261,
+    0.00505776, 0.0365912, 0.0304586, 0.0700241, 0.0725846,
+    0.0176274, 0.000931088, 0.0599891, 0.0608684, 0.087482,
+    0.0299299, 0.0110093, 0.0220128, 0.00194839, 0.02278,
+    0.000614978
   };
   
-
   std::vector<double> solved_frequency(26);
 
   double min_average = INT_MAX;
-  //double total = 0;
   double current_average = 0;
   int rotation = 0;
   for(int i = 0; i < 26; i++)
   {
-    //int present_letters = 0;
     double total = 0;
     std::string message = encryptCaesar(encrypted_string, i);
     solved_frequency = solve_frequency(message);
@@ -54,26 +44,16 @@ std::string solve(std::string encrypted_string)
       }
     }
     current_average = total/26;
-    //std::cout << current_average << std::endl;
     if(current_average < min_average)
     {
       min_average = current_average;
       rotation = i;
-      std::cout << rotation << std::endl;
+      //std::cout << rotation << std::endl;
     }
   }
 
   std::string result = encryptCaesar(encrypted_string, rotation);
   return result;
-  
-  
-  /*
-  current_average = total/26;
-  if(current_average < min_average)
-  {
-    min_average = current_average;
-  }
-  */
 }
 
 
